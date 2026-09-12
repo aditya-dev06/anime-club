459466b6158fdfe1295365effd3a0a3de7d0ff8b
aditya-dev06 <aditya-dev06@users.noreply.github.com>
2026-09-12 18:36:09 +0530

    deploy: update live site with authentic Satella voice and Call of the Witch

--- src/lib/audio.ts
+++
@@ -1,9 +0,0 @@
-import { audioManager } from './audioManager';
-
-export const playSound = (src: string, volume = 1, delay = 0) => {
-  audioManager.play(src, { volume, delay });
-};
-
-export const stopAllSounds = () => {
-  audioManager.stopAll();
-};
